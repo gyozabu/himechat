8fe13b384b785d37d4c4e6fe4b9119055ae8d007
yutoyama <tymy1230@gmail.com>
2019-12-07 17:12:02 +0900

    add js

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -8,24 +8,25 @@
 
       <div class="chat">
         <div class="balloon1-left">
-          <p id="string">僕は、すごく心配だ、よ(T_T)💔😰(￣Д￣；；そんなときは、美味しいもの食べて、元気出さなきゃだね😚😘😄オイラは美陽チャン一筋ダヨ（￣▽￣）💤😪🙂</p>
+          <p id="target">僕は、すごく心配だ、よ(T_T)💔😰(￣Д￣；；そんなときは、美味しいもの食べて、元気出さなきゃだね😚😘😄オイラは美陽チャン一筋ダヨ（￣▽￣）💤😪🙂</p>
         </div>
 
           <form class="form-inline justify-content-center">
             <label class="sr-only" for="inlineFormInputName2">名前</label>
             <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" placeholder="name">
             <button type="submit" class="btn btn-primary mb-2">生成</button><br>
-            <input type="range" id="emoji_range" min="0" max="10" step="1" v-model="emoji_slider"><p>絵文字レベル : {{ emoji_slider }}/p>
-            <input type="range" id="ten_range" min="0" max="10" step="1" value="0"><p>句読点レベル : <span id="num2">0</span></p>
+            <input type="range" id="emoji_range" min="0" max="10" step="1" v-model="emoji_slider"><p>絵文字レベル : {{ emoji_slider }}</p>
+            <input type="range" id="ten_range" min="0" max="10" step="1" v-model="kutouten_slider"><p>句読点レベル : {{kutouten_slider}}</p>
           </form>
 
 
         <div class="sns mt-3">
           <button class="btn btn-outline-dark mr-3">Twitterで送信する</button>
           <button class="btn btn-outline-dark">LINEで送信する</button>
-          <input id="copyTarget" type="text" value="コピー対象の文言" readonly>
-          <button onclick="copyToClipboard()">Copy text</button>
         </div>
+        <button icon fab color="orange" v-on:click="witeToClipboard">
+          <v-icon>copy</v-icon>
+        </button>
       </div>
 
     </div>
@@ -37,7 +38,15 @@ export default {
   name: 'HelloWorld',
   data: function() {
     return {
-      emoji_slider: 1,
+      emoji_slider: 3,
+      kutouten_slider: 3
+    }
+  },
+  methods:{
+    witeToClipboard() {
+      const copyText = this.$el.querySelector('#target').textContent
+      navigator.clipboard
+        .writeText(copyText)
     }
   }
 }
@@ -47,25 +56,8 @@ export default {
 //   return string.innerHTML;
 // }
 
-// var elem = document.getElementById('emoji_range');
-// var target = document.getElementById('num1');
-// var rangeValue = function (elem, target) {
-//     return function(evt){
-//       target.innerHTML = elem.value;
-//     }
-//   }
-//   elem.addEventListener('input', rangeValue(elem, target));
 
-//   function copyToClipboard() {
-//             // コピー対象をJavaScript上で変数として定義する
-//             var copyTarget = document.getElementById("copyTarget");
-//             // コピー対象のテキストを選択する
-//             copyTarget.select();
-//             // 選択しているテキストをクリップボードにコピーする
-//             document.execCommand("Copy");
-//             // コピーをお知らせする
-//             alert("コピーできました！ : " + copyTarget.value);
-//         }
+  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
