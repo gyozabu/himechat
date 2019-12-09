7ff1d76fba91c3fb5f31c117a950202e047c8c3e
TsukadaTomoya <xftpt.nonoka@gmail.com>
2019-12-09 15:12:07 +0900

    ツイートにhimechatのリンク追加

--- front/src/components/HelloWorld.vue
+++ front/src/components/HelloWorld.vue
@@ -130,7 +130,7 @@ export default {
       window.open().location.href =
         "https://twitter.com/intent/tweet?text=" +
         this.chat +
-        "&hashtags=himechat";
+        "&hashtags=himechat"+" https://himechat-gyoza.web.app";
     },
     line: function() {
       window.open().location.href = "https://line.me/R/msg/text/?" + this.chat;
